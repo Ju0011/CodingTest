d195960e9dda13e019f596f5faf7e31326ef4b74
Ju0011 <yhs07240@naver.com>
2024-07-02 22:41:09 +0900

    PCCP_Lv02

--- Lv02/카펫.py
+++ Lv02/카펫.py
@@ -16,3 +16,12 @@ def solution(brown, yellow):
 
 
     return answer
+
+
+# def solution(brown, yellow):
+#     for i in range(1, yellow +1):
+#         if (yellow % i == 0):
+#             yellow_row = int(yellow / i)
+#             yellow_col = i
+#             if (2 * (yellow_row + yellow_col) + 4 == brown):
+#                 return [yellow_row +2, yellow_col+2]
