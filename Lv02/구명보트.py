1e97beed67e7571c1f5986068e95c4de89dedecb
Ju0011 <yhs07240@naver.com>
2024-07-03 20:15:06 +0900

    PCCP_Lv02

---
+++ Lv02/구명보트.py
@@ -0,0 +1,16 @@
+def solution(people, limit):
+    count = 0
+    _people = sorted(people)
+
+    while len(_people) > 1:
+        if _people[0] + _people[-1] <= limit:  # 최댓값과 최솟값 묶어서 보트태움
+            count += 1
+            _people.pop(0)  # 최소 빼내고
+            _people.pop(-1)  # 최대 빼내고
+        else:
+            count += 1
+            _people.pop(-1)
+
+    if _people:  # _people에 1명 남아있는 경우 처리
+        count += 1
+    return count
