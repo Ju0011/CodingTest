7203bbeebfe8821d85d64b543b6166232a5264c6
Ju0011 <yhs07240@naver.com>
2024-09-05 18:40:06 +0900

    test

--- test.py
+++ test.py
@@ -1,12 +1,10 @@
-def solution(numbers, hand):
-    answer = ''
-    a = {1:'L', 3:'R', 4:'L', 6:'R', 7:'L', 9:'R'}
-    for i in numbers:
-        if i in a:
-            answer += a[i]
+def solution(nums):
+    num_set = len(set(nums))
+    N = len(nums) / 2
 
-
-
-    return answer
+    if N > num_set:
+        return num_set
+    else:
+        return int(N)
 
 print(solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5],"right"))
