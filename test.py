67cb593cc87722a7698fc154941ab3df3624b3ea
Ju0011 <yhs07240@naver.com>
2024-10-01 01:54:38 +0900

    test

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
-import itertools
+def solution(cacheSize, cities):
+    answer = 0
+    return answer
 
-n, m = map(int, input().split())
-nums = [i for i in range(1, n+1)]
-
-array = itertools.permutations(nums, m)
-
-for i in array:
-    for j in i:
-        print(j, end = ' ')
-    print()
-
+print(3	,["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"])
